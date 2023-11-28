ff8fcaab435b892db203d0d7e64ac79b80125529
NovichokUA <zubr7333@gmail.com>
2023-11-28 09:45:03 +0200

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,8 @@
 // Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка
 // елемента(тегу < h2 >) і кількість елементів у категорії(усіх < li >, вкладених у нього).
 `use strict`;
-const list = document.querySelector("#categories");
-const listItem = document.querySelectorAll(".item");
+const list = document.querySelector('#categories');
+const listItem = document.querySelectorAll('.item');
 console.log(`Nuber of categories: ${listItem.length}`);
 function result(array) {
   for (const item of array) {
